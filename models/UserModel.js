dea5dc0e7fc2469138114b4d14feff6881ebd3f3
Thiago <thifiorelli@gmail.com>
2019-11-09 00:38:06 -0300

    Add registro de usuarios backend

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true
@@ -19,8 +19,8 @@ const userSchema = new mongoose.Schema({
   },
   date: {
     type: Date,
-    default: Date.Now
+    default: Date.now
   }
 });
 
-module.exports = User = mongoose.model('user', userSchema);
+module.exports = User = mongoose.model('user', UserSchema);
